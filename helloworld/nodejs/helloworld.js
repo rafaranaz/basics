9d8ba1601b0dee0e8a91e6aba97681334108e62a
Rafael Aranaz <raranaz@paradigmadigital.com>
2018-01-21 13:55:04 +0100

    resolucion content-type erroneo. Issue #1

--- helloworld/nodejs/helloworld.js
+++ helloworld/nodejs/helloworld.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type':'test/plain'});
+	response.writeHead(200, {'Content-Type':'text/plain'});
 	response.end('Hello World \n');
 }).listen(8080);
 
